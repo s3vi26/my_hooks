4a620aa7f2d842b9253fbe84d9d8b082ca99033e
Nicole Sevillano <s3vi26@outlook.com>
2019-10-18 13:16:37 -0700

    adding back config file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+      AUTH0_DOMAIN: 'jopeconnect.auth0.com',
+      AUTH0_CLIENT_ID: '3M8APxX9SssH8ai6D0AXzUeQ23llGgGp',
+      REDIRECT_URI: 'http://localhost:3000/',
+      POST_LOGOUT_REDIRECT_URI: 'http://localhost:3000/'
+    }
+  }
