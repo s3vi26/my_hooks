01e8c35e46ebf4019ba6de5cd678c2c3b0f53cde
Nicole Sevillano <s3vi26@outlook.com>
2019-10-18 10:28:15 -0700

    removed congif

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-      AUTH0_DOMAIN: 'jopeconnect.auth0.com',
-      AUTH0_CLIENT_ID: '3M8APxX9SssH8ai6D0AXzUeQ23llGgGp',
-      REDIRECT_URI: 'https://myhooks.s3vi26.now.sh/',
-      POST_LOGOUT_REDIRECT_URI: 'https://myhooks.s3vi26.now.sh/'
-    }
-  }
