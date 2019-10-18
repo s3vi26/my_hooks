82b74dc341ef6537668244fb3cdc77797f9e1442
Nicole Sevillano <s3vi26@outlook.com>
2019-10-18 13:26:58 -0700

    reconfiged file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
       AUTH0_DOMAIN: 'jopeconnect.auth0.com',
       AUTH0_CLIENT_ID: '3M8APxX9SssH8ai6D0AXzUeQ23llGgGp',
-      REDIRECT_URI: 'http://localhost:3000/',
-      POST_LOGOUT_REDIRECT_URI: 'http://localhost:3000/'
+      REDIRECT_URI: 'https://myhooks.s3vi26.now.sh',
+      POST_LOGOUT_REDIRECT_URI: 'https://myhooks.s3vi26.now.sh'
     }
   }
