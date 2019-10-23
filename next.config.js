1c603c6d626cf743f64a789815f86d222ac423ac
Nicole Sevillano <s3vi26@outlook.com>
2019-10-23 09:08:47 -0700

    removed keys from repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
-      AUTH0_DOMAIN: 'jopeconnect.auth0.com',
-      AUTH0_CLIENT_ID: '3M8APxX9SssH8ai6D0AXzUeQ23llGgGp',
+      AUTH0_DOMAIN: '',
+      AUTH0_CLIENT_ID: '',
       REDIRECT_URI: 'https://myhooks.s3vi26.now.sh',
       POST_LOGOUT_REDIRECT_URI: 'https://myhooks.s3vi26.now.sh'
     }
